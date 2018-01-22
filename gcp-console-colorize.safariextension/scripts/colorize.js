59cb729dff5b39aec530841fd191840788f964e8
delphinus <delphinus@remora.cx>
2018-01-23 08:49:16 +0900

    Refresh immediately if config changed

--- gcp-console-colorize.safariextension/scripts/colorize.js
+++ gcp-console-colorize.safariextension/scripts/colorize.js
@@ -1,40 +1,41 @@
 (() => {
-    if (window.top !== window) {
+    const cLog = mes => console.log(`[GCP Console Colorize]: ${mes}`);
+    const targetHost = 'console.cloud.google.com';
+    if (window.top !== window || window.location.host !== targetHost) {
         return;
     }
     const red = 'rgb(218, 67, 54)';
     const blue = 'rgb(59, 120, 231)';
     const headerSelector = '.p6n-system-bar';
     const projectNameSelector = '.p6n-project-switcher-project-name';
-    const colorize = () => {
+    const colorize = keyPhrases => {
+        if (!keyPhrases) {
+            return;
+        }
+        const header = document.querySelector(headerSelector);
+        if (!header) {
+            return;
+        }
+        const projectName = header.querySelector(projectNameSelector);
+        if (!projectName){
+            return;
+        }
+        const projectNameValue = projectName.textContent;
+        const toColorize = keyPhrases.split(/,/).some((current, index, array) => {
+            return projectNameValue.indexOf(current) !== -1;
+        });
+        if (toColorize) {
+            header.style.backgroundColor = red;
+        } else {
+            header.style.backgroundColor = blue;
+        }
+    }
+    window.setInterval(() => {
         safari.self.addEventListener('message', messageEvent => {
-            if (messageEvent.name !== 'keyPhrases') {
-                return;
-            }
-            const keyPhrases = messageEvent.message;
-            if (!keyPhrases) {
-                return;
-            }
-            const header = document.querySelector(headerSelector);
-            if (!header) {
-                return;
-            }
-            const projectName = header.querySelector(projectNameSelector);
-            if (!projectName){
-                return;
-            }
-            const projectNameValue = projectName.textContent;
-            const toColorize = keyPhrases.split(/,/).some((current, index, array) => {
-                console.log(`current: ${current}`);
-                return projectNameValue.indexOf(current) !== -1;
-            });
-            if (toColorize) {
-                header.style.backgroundColor = red;
-            } else {
-                header.style.backgroundColor = blue;
+            if (messageEvent.name === 'keyPhrases') {
+                colorize(messageEvent.message);
             }
         });
         safari.self.tab.dispatchMessage('getKeyPhrases');
-    };
-    window.setInterval(colorize, 1000);
+    }, 1000);
 })();
--- gcp-console-colorize.safariextension/scripts/global.js
+++ gcp-console-colorize.safariextension/scripts/global.js
@@ -1,7 +1,8 @@
 (() => {
-    const keyPhrases = safari.extension.settings.keyPhrases;
+    const cLog = mes => console.log(`[GCP Console Colorize]: ${mes}`);
     safari.application.addEventListener('message', messageEvent => {
         if (messageEvent.name === 'getKeyPhrases') {
+            const keyPhrases = safari.extension.settings.keyPhrases;
             messageEvent.target.page.dispatchMessage('keyPhrases', keyPhrases);
         }
     });
