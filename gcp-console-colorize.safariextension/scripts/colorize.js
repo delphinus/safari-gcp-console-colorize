ba307b3f1198fbd21e48e62463c39ca261165e70
delphinus <delphinus@remora.cx>
2018-05-22 18:30:30 +0900

    Add selectors to detect new UI (v0.4.0)

--- gcp-console-colorize.safariextension/scripts/colorize.js
+++ gcp-console-colorize.safariextension/scripts/colorize.js
@@ -5,37 +5,48 @@
     }
     const white = 'rgb(255, 255, 255)';
     const blue = 'rgb(59, 120, 231)';
-    const headerSelector = '.p6n-system-bar';
-    const projectNameSelector = '.p6n-project-switcher-project-name';
+    const selectors = [{
+            header: '.p6n-system-bar',
+            name: '.p6n-project-switcher-project-name'
+        },
+        {
+            header: '.pcc-platform-bar-container',
+            name: '.cfc-switcher-button-label'
+        }
+    ];
     window.setInterval(
-        () => Events.call({action: 'get-options'}, options => {
-            const header = document.querySelector(headerSelector);
-            if (!header) {
-                return;
-            }
-            const projectName = header.querySelector(projectNameSelector);
-            if (!projectName){
-                return;
-            }
-            const projectNameValue = projectName.textContent;
-            const useRegexp = !!options.useRegexp;
-            let phrases;
-            try {
-                phrases = JSON.parse(options.phrases);
-            } catch (e) {
-                phrases = [];
-            }
-            if (phrases.length === 0) {
-                projectName.style.color = white;
-                header.style.backgroundColor = blue;
-                return;
-            }
-            phrases.some(phrase => {
-                const text = phrase.text.trim();
-                const toColorize = useRegexp ? RegExp(text).test(projectNameValue) : projectNameValue.indexOf(text) !== -1;
-                projectName.style.color = toColorize ? phrase.fgColor : white;
-                header.style.backgroundColor = toColorize ? phrase.bgColor : blue;
-                return toColorize;
+        () => Events.call({
+            action: 'get-options'
+        }, options => {
+            selectors.forEach(selector => {
+                const header = document.querySelector(selector.header);
+                if (!header) {
+                    return;
+                }
+                const projectName = document.querySelector(selector.name);
+                if (!projectName) {
+                    return;
+                }
+                const projectNameValue = projectName.textContent;
+                const useRegexp = !!options.useRegexp;
+                let phrases;
+                try {
+                    phrases = JSON.parse(options.phrases);
+                } catch (e) {
+                    phrases = [];
+                }
+                if (phrases.length === 0) {
+                    projectName.style.color = white;
+                    header.style.backgroundColor = blue;
+                    return;
+                }
+                phrases.some(phrase => {
+                    const text = phrase.text.trim();
+                    const toColorize = useRegexp ? RegExp(text).test(projectNameValue) : projectNameValue.indexOf(text) !== -1;
+                    projectName.style.color = toColorize ? phrase.fgColor : white;
+                    header.style.backgroundColor = toColorize ? phrase.bgColor : blue;
+                    return toColorize;
+                });
             });
         }),
         200);
