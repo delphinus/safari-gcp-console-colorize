46eb3598e07e3f19fb59ac843bf1e6addb80157f
delphinus <delphinus@remora.cx>
2018-05-23 13:28:41 +0900

    Clean up code

--- gcp-console-colorize.safariextension/scripts/colorize.js
+++ gcp-console-colorize.safariextension/scripts/colorize.js
@@ -14,40 +14,40 @@
             name: '.cfc-switcher-button-label'
         }
     ];
+    const colorize = (selector, options) => {
+        const header = document.querySelector(selector.header);
+        if (!header) {
+            return;
+        }
+        const projectName = document.querySelector(selector.name);
+        if (!projectName) {
+            return;
+        }
+        const projectNameValue = projectName.textContent;
+        const useRegexp = !!options.useRegexp;
+        let phrases;
+        try {
+            phrases = JSON.parse(options.phrases);
+        } catch (e) {
+            phrases = [];
+        }
+        if (phrases.length === 0) {
+            projectName.style.color = white;
+            header.style.backgroundColor = blue;
+            return;
+        }
+        return phrases.some(phrase => {
+            const text = phrase.text.trim();
+            const toColorize = useRegexp ? RegExp(text).test(projectNameValue) :
+                projectNameValue.includes(text);
+            projectName.style.color = toColorize ? phrase.fgColor : white;
+            header.style.backgroundColor = toColorize ? phrase.bgColor : blue;
+            return toColorize;
+        });
+    };
     window.setInterval(
         () => Events.call({
             action: 'get-options'
-        }, options => {
-            selectors.forEach(selector => {
-                const header = document.querySelector(selector.header);
-                if (!header) {
-                    return;
-                }
-                const projectName = document.querySelector(selector.name);
-                if (!projectName) {
-                    return;
-                }
-                const projectNameValue = projectName.textContent;
-                const useRegexp = !!options.useRegexp;
-                let phrases;
-                try {
-                    phrases = JSON.parse(options.phrases);
-                } catch (e) {
-                    phrases = [];
-                }
-                if (phrases.length === 0) {
-                    projectName.style.color = white;
-                    header.style.backgroundColor = blue;
-                    return;
-                }
-                phrases.some(phrase => {
-                    const text = phrase.text.trim();
-                    const toColorize = useRegexp ? RegExp(text).test(projectNameValue) : projectNameValue.indexOf(text) !== -1;
-                    projectName.style.color = toColorize ? phrase.fgColor : white;
-                    header.style.backgroundColor = toColorize ? phrase.bgColor : blue;
-                    return toColorize;
-                });
-            });
-        }),
+        }, options => selectors.some(selector => colorize(selector, options))),
         200);
 })();
